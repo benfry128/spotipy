4915a1c78f5ba9a5c1f04d8631603bf74db1da07
benfry128 <benfry128@gmail.com>
2024-07-26 00:21:03 -0400

    Added code to merge 2 versions of the same album together (manual process but it works)

--- db_loop.py
+++ db_loop.py
@@ -4,7 +4,7 @@ sp = utils.spotipySetup()
 
 (db, cursor) = utils.db_setup()
 
-# cursor.execute('SELECT * from tracks where id > 2282 order by id ')
+# cursor.execute('SELECT * from tracks where id < 2283 order by id ')
 
 # tracks = cursor.fetchall()
 
@@ -29,44 +29,63 @@ sp = utils.spotipySetup()
 #             cursor.execute('INSERT INTO albums (url, title, type) VALUES (%s, %s, %s)', (url, name, album_type))
 #         db.commit()
 
-cursor.execute('SELECT * from albums where type = "single" order by url')
+# cursor.execute('SELECT * from albums where type = "single" order by url')
+
+# albums = cursor.fetchall()
+
+# for a in albums:
+#     print("")
+#     print(a[1])
+#     cursor.execute('select * from tracks where album = %s', [a[0]])
+#     tracks = cursor.fetchall()
+#     for t in tracks:
+#         print(t[1])
+#         possible_tracks = sp.search(q=f'track:{t[1]} artist:{t[2]}', type='track', limit=10)['tracks']['items']
+#         for track in possible_tracks:
+#             if track['album']['album_type'] == 'album':
+#                 print(f"{track['name']} by {track['artists'][0]['name']} off {track['album']['name']}. url is {track['external_urls']['spotify']}")
+
+#         url = input('Which url? or enter an int')
+
+#         if not url:
+#             continue
+
+#         good_track = sp.track(url)
+#         input(good_track['album']['name'])
+#         album_url = good_track['album']['external_urls']['spotify']
+
+#         cursor.execute('select * from tracks where url = %s', [url])
+#         old_record = cursor.fetchone()
+#         if old_record:
+#             utils.merge_tracks(old_record[0], t[0], db, cursor)
+#             continue
+
+#         cursor.execute('select * from albums where url = %s', [album_url])
+#         old_album = cursor.fetchone()
+#         if not old_album:
+#             input(f"about to put in a new album: {good_track['album']['name']}, it's a {good_track['album']['album_type']}")
+#             cursor.execute('INSERT INTO albums (url, title, type) VALUES (%s, %s, %s)', (album_url, good_track['album']['name'], good_track['album']['album_type']))
+
+#         input(f"about to update this track's album ok? {url} {album_url}")
+#         cursor.execute('update tracks set url = %s, album = %s where id = %s', (url, album_url, t[0]))
+#         db.commit()
+
+''''''
+
+cursor.execute("select album from tracks where album not like '%spotify%' group by album;")
 
 albums = cursor.fetchall()
 
-for a in albums:
-    print("")
-    print(a[1])
-    cursor.execute('select * from tracks where album = %s', [a[0]])
-    tracks = cursor.fetchall()
-    for t in tracks:
-        print(t[1])
-        possible_tracks = sp.search(q=f'track:{t[1]} artist:{t[2]}', type='track', limit=10)['tracks']['items']
-        for track in possible_tracks:
-            if track['album']['album_type'] == 'album':
-                print(f"{track['name']} by {track['artists'][0]['name']} off {track['album']['name']}. url is {track['external_urls']['spotify']}")
+for album in albums:
+    print(album[0])
+    
 
-        url = input('Which url? or enter an int')
-
-        if not url:
-            continue
-
-        good_track = sp.track(url)
-        input(good_track['album']['name'])
-        album_url = good_track['album']['external_urls']['spotify']
-
-        cursor.execute('select * from tracks where url = %s', [url])
-        old_record = cursor.fetchone()
-        if old_record:
-            utils.merge_tracks(old_record[0], t[0], db, cursor)
-            continue
-
-        cursor.execute('select * from albums where url = %s', [album_url])
-        old_album = cursor.fetchone()
-        if not old_album:
-            input(f"about to put in a new album: {good_track['album']['name']}, it's a {good_track['album']['album_type']}")
-            cursor.execute('INSERT INTO albums (url, title, type) VALUES (%s, %s, %s)', (album_url, good_track['album']['name'], good_track['album']['album_type']))
-
-        input(f"about to update this track's album ok? {url} {album_url}")
-        cursor.execute('update tracks set url = %s, album = %s where id = %s', (url, album_url, t[0]))
-        db.commit()
-        
+    # print(track[1])
+    # print(track[0])
+    # # print(track[3])
+    # # title = track[3]
+    # # print(track[4])
+    # # url = input("URL? ")
+    # # cursor.execute('INSERT INTO albums (url, title, type) VALUES (%s, %s, %s);', [url, title, 'single'])
+    # cursor.execute('UPDATE tracks SET album = %s WHERE id = %s', [track[1], track[0]])
+    # db.commit()
--- test.py
+++ test.py
@@ -4,4 +4,57 @@ sp = utils.spotipySetup()
 
 (db, cursor) = utils.db_setup()
 
-utils.merge_tracks(2437, 256, db, cursor)
+# code to merge 2 albums
+
+urls = ['https://open.spotify.com/album/5MqyhhHbT13zsloD3uHhlQ', 'https://open.spotify.com/album/53PBYiedQrASAs5sy63JqT']
+
+titles = []
+album_dicts = []
+total_tracks = []
+
+for url in urls:
+    cursor.execute('SELECT * FROM tracks WHERE album = %s', [url])
+    titles.append([song[1] for song in cursor.fetchall()])
+    tracks = sp.album_tracks(url)['items']
+    track_titles = [track['name'] for track in tracks]
+    track_urls = [track['external_urls']['spotify'] for track in tracks]
+    album_dicts.append(dict(zip(track_titles, track_urls)))
+
+
+for t in titles:
+    for ti in t:
+        print(ti)
+input('Do you need to go and merge some tracks first????')
+
+
+for (url, album_dict, s) in zip(urls, album_dicts, titles):
+    print(f"Let's see about this url: {url}")
+    print(f'We have {len(s)} tracks in the db related to this url')
+    good = True
+    for title_set in titles:
+        for title in title_set:
+            if title not in album_dict:
+                print(f"couldn't find {title}")
+                good = False
+                break
+        if not good:
+            break
+    if good:
+        print("OK THAT'S GOOD!")
+    else:
+        print("NAH DON'T USE THIS ONE PROB")
+
+
+i = input("Ok you want to do it? Choose the index of the album you want to keep, or press enter to skip")
+if i:
+    index = int(i)
+    good_url = urls[index]
+    album_dict = album_dicts[index]
+    del urls[index]
+    del titles[index]
+    for (url, title_set) in zip(urls, titles):
+        for title in title_set:
+            cursor.execute('UPDATE tracks SET album = %s, url = %s where name = %s and album = %s', (good_url, album_dict[title], title, url))
+            db.commit()
+
+    # do the merging here
