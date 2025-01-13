3d9acb5e1478823c46c7e519665c66b5b986a3ff
benfry128 <benfry128@gmail.com>
2025-01-12 23:08:54 -0500

    Updated song_chart to get fuull year song chart

--- song_chart.py
+++ song_chart.py
@@ -1,14 +1,13 @@
 import utils
 from datetime import datetime
-from pprint import pprint
 
 sp = utils.spotipy_setup()
 
 (db, cursor) = utils.db_setup()
 
 for i in range(1, 12):
-    start_time = datetime(2024, i, 1)
-    end_time = datetime(2024, i+1, 1)
+    start_time = datetime(2024, 1, 1)
+    end_time = datetime(2025, 1, 1)
 
     start_timestamp = start_time.timestamp()
     end_timestamp = end_time.timestamp()
@@ -18,10 +17,10 @@ for i in range(1, 12):
             join tracks on tracks.id = scrobbles.track_id
             join albums_url on albums_url.id = tracks.album_id
         where utc > %s and utc < %s
-        group by albums_url.id order by count(*) desc limit 12;
+        group by albums_url.id order by count(*) desc limit 36;
         ''', [start_timestamp, end_timestamp])
 
     album_info = [row[0] for row in cursor.fetchall()]
 
-    utils.compile_square_image(3, 4, 640, album_info)
+    utils.compile_square_image(6, 6, 640, album_info)
     input("HI")
